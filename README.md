3e520a15aa25c38dd25fca576fdcfff08cf950d0
eggplants <w10776e8w@yahoo.co.jp>
2021-06-16 14:58:56 +0900

    Create: curl service

--- README.md
+++ README.md
@@ -1,18 +1,3 @@
 # pop
 
-## Requirement
-
-```bash
-wget https://v.redd.it/mhogbpazd1s51/DASH_360.mp4
-ffmpeg -i DASH_360.mp4 -vf fps=1 out%d.png
-# upload out{1,2}.png to remove.bg, then...
-pip install i2a
-i2a out1-re* > o1
-i2a out2-re* > o2
-```
-
-## Pop on terminal
-
-```bash
-./pop
-```
+- popcat from curl
---
+++ index.js
@@ -0,0 +1,106 @@
+const fs = require('mz/fs');
+const path = require('path');
+const http = require('http');
+const url = require('url');
+const { Readable } = require('stream');
+const colors = require('colors/safe');
+
+// Setup frames in memory
+let original;
+let flipped;
+
+(async () => {
+  const framesPath = 'frames';
+  const files = await fs.readdir(framesPath);
+
+  original = await Promise.all(files.map(async (file) => {
+    const frame = await fs.readFile(path.join(framesPath, file));
+    return frame.toString();
+  }));
+  flipped = original.map(f => {
+    return f
+      .toString()
+      .split('')
+      .reverse()
+      .join('')
+  })
+})().catch((err) => {
+  console.log('Error loading frames');
+  console.log(err);
+});
+
+const colorsOptions = [
+  'red',
+  'yellow',
+  'green',
+  'blue',
+  'magenta',
+  'cyan',
+  'white'
+];
+const numColors = colorsOptions.length;
+const selectColor = previousColor => {
+  let color;
+
+  do {
+    color = Math.floor(Math.random() * numColors);
+  } while (color === previousColor);
+
+  return color;
+};
+
+const wait = 200;
+
+const streamer = (stream, opts) => {
+  let index = 0;
+  let lastColor;
+  let frame = null;
+  const frames = opts.flip ? flipped : original;
+
+  return setInterval(() => {
+    // clear the screen
+    stream.push('\033[2J\033[3J\033[H');
+
+    const newColor = lastColor = selectColor(lastColor);
+
+    stream.push(colors[colorsOptions[newColor]](frames[index]));
+
+    index = (index + 1) % frames.length;
+  }, wait);
+};
+
+const validateQuery = ({ flip }) => ({
+  flip: String(flip).toLowerCase() === 'true'
+});
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/healthcheck') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    return res.end(JSON.stringify({status: 'ok'}));
+  }
+
+  if (
+    req.headers &&
+    req.headers['user-agent'] &&
+    !req.headers['user-agent'].includes('curl')
+  ) {
+    res.writeHead(302, { Location: 'https://github.com/hugomd/parrot.live' });
+    return res.end();
+  }
+
+  const stream = new Readable();
+  stream._read = function noop() {};
+  stream.pipe(res);
+  const interval = streamer(stream, validateQuery(url.parse(req.url, true).query));
+
+  req.on('close', () => {
+    stream.destroy();
+    clearInterval(interval);
+  });
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, err => {
+  if (err) throw err;
+  console.log(`Listening on localhost:${port}`);
+});
